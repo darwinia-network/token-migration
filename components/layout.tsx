1edb6cbf220ac12306c4a3c20898a972a31f301c
JayJay1024 <lygm1062@gmail.com>
2022-08-10 17:59:26 +0800

    Update content and optimize style

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,9 +4,9 @@ import Header from "./header";
 import Footer from "./footer";
 
 const Layout = ({ children }: PropsWithChildren<{}>) => (
-  <div className="min-h-screen relative">
+  <div className="min-h-screen relative flex flex-col justify-center">
     <Header />
-    <main className="container mx-auto pb-16">{children}</main>
+    <main className="container mx-auto pt-14 pb-16">{children}</main>
     <Footer />
   </div>
 );
