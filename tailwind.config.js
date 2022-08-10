37bc2005be14747e005fe7ae87f74ad6815574ae
JayJay1024 <lygm1062@gmail.com>
2022-08-10 17:46:40 +0800

    Config container padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      padding: {
+        xl: "8rem",
+        "2xl": "10rem",
+      },
+    },
     extend: {},
   },
   plugins: [],
