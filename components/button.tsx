d612008fd2b20adf2f95c36d0329812cb39e3c68
JayJay1024 <lygm1062@gmail.com>
2022-08-11 00:05:13 +0800

    Button add onClick prop

--- components/button.tsx
+++ components/button.tsx
@@ -3,14 +3,16 @@ import type { PropsWithChildren } from "react";
 interface Props {
   className?: string;
   type?: "default" | "primary";
+  onClick?: () => void;
 }
 
-const Button = ({ children, className, type }: PropsWithChildren<Props>) => {
+const Button = ({ children, className, type, onClick }: PropsWithChildren<Props>) => {
   return (
     <button
       className={`${
         type === "primary" ? "bg-primary" : "border-primary"
       } text-sm leading-7 font-light py-2 px-4 ${className}`}
+      onClick={onClick}
     >
       {children}
     </button>
