60934579ddb7d8ad02dba56ab92acb05051141ec
JayJay1024 <lygm1062@gmail.com>
2022-08-10 18:40:12 +0800

    Update Header height

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const navData = [
 ] as { text: string; link: string }[];
 
 const Header = () => (
-  <div className="h-14 flex flex-col justify-center w-full absolute top-0 left-0">
+  <div className="h-16 flex flex-col justify-center w-full absolute top-0 left-0">
     <div className="container mx-auto flex items-center justify-between">
       <Image src="/images/logo.svg" width={178} height={22} />
       <div className="flex items-center space-x-5">
--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,7 @@ import Footer from "./footer";
 const Layout = ({ children }: PropsWithChildren<{}>) => (
   <div className="min-h-screen relative flex flex-col justify-center">
     <Header />
-    <main className="container mx-auto pt-14 pb-16">{children}</main>
+    <main className="container mx-auto py-16">{children}</main>
     <Footer />
   </div>
 );
