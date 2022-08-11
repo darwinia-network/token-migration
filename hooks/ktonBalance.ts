f8df92a9620e143410b402072f274f1957d93f70
JayJay1024 <lygm1062@gmail.com>
2022-08-12 00:03:41 +0800

    useKtonBalance hook

---
+++ hooks/ktonBalance.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { BigNumber, Contract } from "ethers";
+import { Subscription, from } from "rxjs";
+
+import { useApi } from "./api";
+import ktonAbi from "../abi/ktonABI.json";
+
+export const useKtonBalance = (contractAddress: string, account: string) => {
+  const { provider } = useApi();
+  const [balance, setBalance] = useState(BigNumber.from(0));
+
+  useEffect(() => {
+    let sub$$: Subscription;
+
+    if (contractAddress && account && provider) {
+      const contract = new Contract(contractAddress, ktonAbi, provider);
+
+      sub$$ = from(contract.balanceOf(account) as Promise<BigNumber>).subscribe(setBalance);
+    }
+
+    return () => {
+      if (sub$$) {
+        sub$$.unsubscribe();
+      }
+    };
+  }, [contractAddress, account, provider]);
+
+  return { balance };
+};
