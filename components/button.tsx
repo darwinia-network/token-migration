bca8562590b426ec72ef1d9d2a88ff48c5365cea
JayJay1024 <lygm1062@gmail.com>
2022-08-12 01:47:12 +0800

    Button hover style

--- components/button.tsx
+++ components/button.tsx
@@ -12,7 +12,7 @@ const Button = ({ children, className, type, disable, onClick }: PropsWithChildr
     <button
       className={`${type === "primary" ? "bg-primary" : "border-[2px] border-primary"} ${
         disable ? "hover:cursor-not-allowed opacity-50" : ""
-      } text-sm leading-7 font-light py-2 px-4 ${className}`}
+      } text-sm leading-7 font-light py-2 px-4 hover:bg-white hover:text-black hover:border-white ${className}`}
       onClick={onClick}
       disabled={disable}
     >
