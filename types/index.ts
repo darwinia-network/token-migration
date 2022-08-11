dab216fa972fd2624fa61df6c99b0bcad57386ab
JayJay1024 <lygm1062@gmail.com>
2022-08-11 14:20:11 +0800

    Some types

---
+++ types/index.ts
@@ -0,0 +1,15 @@
+export enum ChainName {
+  CrabSmartChain = "Crab Smart Chain",
+  PangolinSmartChain = "Pangolin Smart Chain",
+}
+
+export enum TokenName {
+  XWRing = "xWRING",
+  XWORing = "xWORING",
+  CKton = "CKTON",
+  PKton = "PKTON",
+  XRingClassic = "xRING(Classic)",
+  XORingClassic = "xORING(Classic)",
+  WCKtonClassic = "WCKTON(Classic)",
+  WPKtonClassic = "WPKTON(Classic)",
+}
