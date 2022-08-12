976d5b76d7c8032c430243f744e35ebb568ca794
Jay <lygm1062@gmail.com>
2022-08-12 12:15:43 +0800

    Fix import header error

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "react";
 
-import Header from "./header";
+import Header from "./Header";
 import Footer from "./footer";
 
 const Layout = ({ children }: PropsWithChildren<{}>) => (
