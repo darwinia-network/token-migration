c9e880d6fe728208e41635df6e0f04d881862f0e
JayJay1024 <lygm1062@gmail.com>
2022-08-23 10:07:20 +0800

    Set WCKTON migration disable

--- src/config/migrators.ts
+++ src/config/migrators.ts
@@ -12,7 +12,7 @@ export const MIGRATORS_CONF: Record<ChainID, Migrator[]> = {
       from: TokenSymbol.WCKtonClassic,
       to: TokenSymbol.CKton,
       contract: "0xD5f4940704Eb4cE5e4b51877d49B58A3e93531b6",
-      disable: false,
+      disable: true,
     },
   ],
   [ChainID.PangolinSmartChain]: [
